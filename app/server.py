c687fdcaadfb120cfe206a4939a582aadf861419
zaryabshahzaib <31673953+zaryabshahzaib@users.noreply.github.com>
2020-02-01 13:04:04 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,48 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1NvSfgH3pl1l36fRcYG8rYYw4-ZqkhwFO'
+export_file_name = 'export_resnet34_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Apple___Apple_scab',
+'Apple___Black_rot',
+'Apple___Cedar_apple_rust',
+'Apple___healthy',
+'background',
+'Blueberry___healthy',
+'Cherry_(including_sour)___healthy',
+'Cherry_(including_sour)___Powdery_mildew',
+'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+'Corn_(maize)___Common_rust_',
+'Corn_(maize)___healthy',
+'Corn_(maize)___Northern_Leaf_Blight',
+'Grape___Black_rot',
+'Grape___Esca_(Black_Measles)',
+'Grape___healthy',
+'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+'Orange___Haunglongbing_(Citrus_greening)',
+'Peach___Bacterial_spot',
+'Peach___healthy',
+'Pepper,_bell___Bacterial_spot',
+'Pepper,_bell___healthy',
+'Potato___Early_blight',
+'Potato___healthy',
+'Potato___Late_blight',
+'Raspberry___healthy',
+'Soybean___healthy',
+'Squash___Powdery_mildew',
+'Strawberry___healthy',
+'Strawberry___Leaf_scorch',
+'Tomato___Bacterial_spot',
+'Tomato___Early_blight',
+'Tomato___healthy',
+'Tomato___Late_blight',
+'Tomato___Leaf_Mold',
+'Tomato___Septoria_leaf_spot',
+'Tomato___Spider_mites Two-spotted_spider_mite',
+'Tomato___Target_Spot',
+'Tomato___Tomato_mosaic_virus',
+'Tomato___Tomato_Yellow_Leaf_Curl_Virus']
 path = Path(__file__).parent
 
 app = Starlette()
